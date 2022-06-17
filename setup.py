9919b9beafe110cfa4224aa028776563d2dc7af7
v-salamdari <sarahhalam@gmail.com>
2022-06-17 10:27:57 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dms", # Replace with your own username
+    version="0.0.1",
+    description="Diffusion Models for Biomolecular Design",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
