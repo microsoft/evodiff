23673b388cd06a00ecb15b3bfa86b84a6b56f199
v-salamdari <sarahhalam@gmail.com>
2022-06-30 09:34:46 -0700

    add austin dm collater

--- dms/collaters.py
+++ dms/collaters.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 from dms.utils import Tokenizer
+from dms.utils import Blosum62
+from sequence_models.constants import STOP
+from dms.constants import PROTEIN_ALPHABET
 
 def _pad(tokenized, value):
     """
@@ -16,6 +19,12 @@ def _pad(tokenized, value):
         output[row, :len(t)] = t
     return output
 
+def _unpad(x, value):
+    x_pad = x.clone()
+    mask_pad = x_pad != value
+    x = x[mask_pad].to(torch.int64)
+    return x
+
 def _normalize_seq_lengths(tokenized, seq_length, value):
     """
     Utility function that pads batches to the same length.
@@ -27,13 +36,47 @@ def _normalize_seq_lengths(tokenized, seq_length, value):
     """
     batch_size = len(tokenized)
     output = torch.zeros((batch_size, seq_length)) + value
-    print(output.shape)
     for row, t in enumerate(tokenized):
         if len(t) <= seq_length:
             output[row, :len(t)] = t
         else:
             output[row, :seq_length] = t[:seq_length]
     return output
+
+def _beta_schedule(num_timesteps, schedule='linear', start=1e-5, end=0.999):
+    """
+    Variance schedule for adding noise as introduced by Nichol and Dhariwal and adapted by Hoogeboom et al
+    Coined as uniform schedule in Austin et al.
+    Start/End will control the magnitude of sigmoidal and cosine schedules..
+    #TODO: Check that cosine matches Austin cosine schedule - I think theirs is slightly diff
+    #TODO: add mutual information Beta_t introduced by Sohl Dickensen used by Austin
+    """
+    if schedule == 'linear':
+        betas = torch.linspace(start, end, num_timesteps)
+    elif schedule == "quad":
+        betas = torch.linspace(start ** 0.5, end ** 0.5, num_timesteps) ** 2
+    elif schedule == "sigmoid":
+        betas = torch.linspace(-10, 10, num_timesteps)
+        betas = torch.sigmoid(betas) * (end - start) + start
+    elif schedule == "cosine":
+        betas = torch.linspace(np.pi / 2, 0, num_timesteps)
+        betas = torch.cos(betas) * (end - start) + start
+    else:
+        print("Must select a valid schedule; ['linear', 'quad', 'sigmoid', 'cosine']")
+    return betas
+
+def blosum_probability(tokenized):
+    d = Blosum62().blosum_dict()
+    out = torch.zeros((len(tokenized)))
+    for i, x in enumerate(tokenized):
+        if i < len(tokenized) - 1:
+            _tuple = tuple(np.array(tokenized[i:i + 2]))
+        else:
+            end = tokenized[PROTEIN_ALPHABET.index(STOP)]
+            _tuple = tuple(np.array((tokenized[i], end)))
+        out[i] = d[_tuple]
+    return out
+
 
 class SimpleCollater(object):
     """
@@ -62,14 +105,14 @@ class SimpleCollater(object):
         tokenized = [torch.LongTensor(self.tokenizer.tokenize(s)) for s in sequences]
 
         if self.norm:
-            print("Truncating sequences to ", self.seq_length, "and padding")
+            #print("Truncating sequences to ", self.seq_length, "and padding")
             tokenized = _normalize_seq_lengths(tokenized, self.seq_length, self.tokenizer.pad_id)
 
         if self.pad:
-            print("Padding sequences to max seq length")
+            #print("Padding sequences to max seq length")
             tokenized = _pad(tokenized, self.tokenizer.pad_id)
 
-        print("Tokenizing sequences")
+        #print("Tokenizing sequences")
         return (tokenized)
 
 class OAMaskCollater(object):
@@ -102,10 +145,10 @@ class OAMaskCollater(object):
         mask_id = torch.tensor(self.tokenizer.mask_id, dtype=torch.int64)
 
         for i,x in enumerate(tokenized):
-            if self.inputs_padded: # if truncating to some length first, inputs will be padded
+            if self.inputs_padded: # if truncating seqs to some length first in SimpleCollater, inputs will be padded
                  x_pad = x.clone()
                  mask_pad = x_pad != self.tokenizer.pad_id
-                 num_pad = len(x_pad) - mask_pad.sum()
+                 #num_pad = len(x_pad) - mask_pad.sum()
                  x = x[mask_pad].to(torch.int64)
             # Randomly generate timestep and indices to mask
             D = len(x) # D should have the same dimensions as each sequence length
@@ -118,13 +161,91 @@ class OAMaskCollater(object):
             index_arr = np.arange(0, len(x)) #index array [1...seq_len]
             mask = np.isin(index_arr, mask_arr, invert=False).reshape(index_arr.shape) # mask bools indices specified by mask_arr
             # Mask inputs
-            x[mask] = mask_id
+            #x[mask] = mask_id # not appending correctly :(
             mask = torch.tensor(mask, dtype=torch.bool)
-            src.append(x)
             masks.append(mask)
+            #mask = mask.to(torch.long)
+            #print(mask.dtype, x.dtype, mask_id.dtype)
+            x_t = ~mask * x + mask * mask_id
+            #mask = torch.tensor(mask, dtype=torch.bool)
+            src.append(x_t)
+            #masks.append(mask)
         # PAD out
         src = _pad(src, self.tokenizer.pad_id)
         masks = _pad(masks*1, 0)
         tokenized = _pad(tokenized, self.tokenizer.pad_id)
+        print(src.shape, masks.shape)
         return (src.to(torch.long), timesteps, tokenized.to(torch.long), masks)
+
+class DMCollater(object):
+    """
+    Collater for masking sequences:
+    - D3PM according to Austin et al.
+    - OAARDMs according to Hoogeboom et al.
+    inputs:
+        sequences : list of sequences
+        inputs_padded: if inputs are padded (due to truncation in Simple_Collater) set True (default False)
+        scheme: 'OA' or 'D3PM' Order Agnostic or D3PM masking scheme
+
+    OA-ARM variables:
+        D : possible permutations from 0.. max length
+        t : randomly selected timestep
+
+    outputs:
+        src : source  masked sequences (model input)
+        timesteps: (D-t+1) term
+        tokenized: tokenized sequences (target seq)
+        masks: masks used to generate src
+    """
+    def __init__(self, simple_collater, inputs_padded=False, num_timesteps=1000, masking_scheme='random', schedule='cosine'):
+        self.simple_collater = simple_collater
+        self.tokenizer = Tokenizer()
+        self.inputs_padded  = inputs_padded
+        self.num_timesteps = num_timesteps
+        self.masking_scheme = masking_scheme
+        self.schedule = schedule
+
+    def __call__(self, sequences):
+        tokenized = self.simple_collater(sequences)
+        src=[]
+        masks=[]
+        mask_id = torch.tensor(self.tokenizer.mask_id, dtype=torch.int64)
+
+        # Make time schedule
+        if self.masking_scheme == 'random':
+            betas = _beta_schedule(self.num_timesteps, schedule=self.schedule)
+            t_betas = [betas[step] for step in range(self.num_timesteps)]
+        elif self.masking_scheme == 'blosum62':
+            d = Blosum62().blosum_dict()
+            betas = _beta_schedule(self.num_timesteps, schedule=self.schedule, start=min(d.values()), end=max(d.values()))
+            t_betas = [betas[step] for step in range(self.num_timesteps)]
+
+        for i, x in enumerate(tokenized):
+            #print("seq", i)
+            if self.inputs_padded:
+                 x = _unpad(x, self.tokenizer.pad_id)
+            # x_t = x.clone()
+            if self.masking_scheme == 'random':
+                 x_prob = torch.rand_like(x.float())
+            elif self.masking_scheme == 'blosum62':
+                 x_prob = blosum_probability(x.float())
+            else:
+                 print("Select random or blosum62 for masking scheme")
+
+            for t in t_betas:
+                #print("time", t)
+                mask = x_prob < t
+                #x[mask] = mask_id
+                #print(x)
+                x_t = ~mask*x + mask*mask_id
+                #print(x_t)
+                masks.append(mask)
+                src.append(x_t)
+        # PAD outputs
+        src = _pad(src, self.tokenizer.pad_id)
+        #print(src)
+        masks = _pad(masks*1, 0)
+        tokenized = _pad(tokenized, self.tokenizer.pad_id)
+        return (src.to(torch.long), tokenized.to(torch.long), masks)
+
 
