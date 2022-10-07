fix normalization of loss w/ multi-gpu

dms/losses.py
@@ -1,10 +1,10 @@
 import torch
 from torch.nn import CrossEntropyLoss, KLDivLoss
 from dms.utils import Tokenizer
-from sequence_models.constants import MSA_ALPHABET, MSA_AAS
+from sequence_models.constants import MSA_AAS
 from torch.nn.functional import normalize
 
-def sample_prior(a,b, _len=len(MSA_AAS)): # TODO dont need len anymore
+def sample_prior(a,b, _len=len(MSA_AAS)):
     """
     Returns prior for KL at T-> inf with same shape as q over total possible values (all_aas)
     Prior is a stationary distribution; uniform distribution over number of values
@@ -13,7 +13,7 @@ def sample_prior(a,b, _len=len(MSA_AAS)): # TODO dont need len anymore
     prior = torch.ones_like(prior) / _len
     return prior
 
-def sample_prior3D(a,b,c, _len=MSA_AAS): # TODO dont need len anymore
+def sample_prior3D(a,b,c, _len=MSA_AAS):
     """
     Returns prior for KL at T-> inf with same shape as q over total possible values (all_aas)
     Prior is a stationary distribution; uniform distribution over number of values
@@ -22,16 +22,18 @@ def sample_prior3D(a,b,c, _len=MSA_AAS): # TODO dont need len anymore
     prior = torch.ones_like(prior) / len(_len)
     return prior
 
-class MaskedCrossEntropyLoss(CrossEntropyLoss):
+class AutoregMaskedCrossEntropyLoss(CrossEntropyLoss):
     """Masked cross-entropy loss for sequences.
-    Evaluates the cross-entropy loss at specified locations in a sequence, using a reweighting term for diffusion
-    models 1/(D-t+1) output in OAMaskCollater
+    Evaluates the cross-entropy loss at specified locations in a sequence
+    When reweight = True, reweights CE according to Hoogeboom et al.;
+    reweight term = 1/(D-t+1)
     Shape:
         Inputs:
             - pred: (N, L, n_tokens)
             - tgt: (N, L)
             - mask: (N, L) boolean
             - timestep (N, L) output from OAMaskCollater
+            - input mask (N, L)
             - weight: (C, ): class weights for nn.CrossEntropyLoss
 
     Returns
@@ -43,7 +45,6 @@ class MaskedCrossEntropyLoss(CrossEntropyLoss):
         self.tokenizer = tokenizer
         super().__init__(weight=weight, reduction=reduction)
     def forward(self, pred, tgt, mask, timesteps, input_mask):
-        alphabet = self.tokenizer.tokenize([self.tokenizer.alphabet])
         # Make sure we have that empty last dimension
         if len(mask.shape) == len(pred.shape) - 1:
             mask = mask.unsqueeze(-1)
@@ -52,28 +53,21 @@ class MaskedCrossEntropyLoss(CrossEntropyLoss):
         mask = mask.bool()
         input_mask = input_mask.bool() # padded seq
         # Select
-        #n = mask.sum() # len mask tokens
-        mask_tokens = mask.sum(axis=1) # len batch
-        n_tokens = input_mask.sum(axis=1) # len batch
-        nll_losses = 0
-        ce_losses = []
-        for i in range(tgt.shape[0]): # iterate over each sequence in batch
-            p = torch.masked_select(pred[i], mask[i]).view(mask_tokens[i], len(alphabet)) # predictions for each mask
-            t = torch.masked_select(tgt[i], mask[i].squeeze())#.squeeze())
-            loss = super().forward(p, t)
-            #print("loss", loss)
-            if self.reweight: # Uses autoreg reweighting term
-                # Reweight for summation over t
-                _timesteps = timesteps[i].repeat_interleave(timesteps[i])
-                rwt_term = 1. / _timesteps  # Hoogeboom OARDM
-                _n_tokens = torch.repeat_interleave(n_tokens[i], len(loss), axis=0)
-                ce_loss = _n_tokens * rwt_term * loss
-            if not self.reweight:  # For D3PM reweight in train loop
-                ce_loss = loss
-            ce_losses.append(ce_loss.sum()) # reduce mean
-            nll_losses += loss.sum()
-        nll_losses = nll_losses/mask.sum()
-        ce_losses = torch.stack(ce_losses, dim=0).sum()/n_tokens.sum() # divide by D so we can compare to bits per char
+        mask_tokens = mask.sum() # masked tokens
+        nonpad_tokens = input_mask.sum(dim=1) # nonpad tokens
+        p = torch.masked_select(pred, mask).view(mask_tokens, -1) # [T x K] predictions for each mask char
+        t = torch.masked_select(tgt, mask.squeeze()) # [ T ] true mask char
+        loss = super().forward(p, t) # [ T ] loss per mask char
+        # Calculate reweighted CE loss and NLL loss
+        nll_losses = loss.sum()
+        if self.reweight: # Uses Hoogeboom OARDM reweighting term
+            rwt_term = 1. / timesteps
+            rwt_term = rwt_term.repeat_interleave(timesteps)
+            _n_tokens = nonpad_tokens.repeat_interleave(timesteps)
+            ce_loss = _n_tokens * rwt_term * loss
+            ce_losses = ce_loss.sum()  # reduce mean
+        else:
+            ce_losses = nll_losses
         return ce_losses, nll_losses.to(torch.float64)
 
 
@@ -153,23 +147,9 @@ class D3PMLVBLoss(KLDivLoss):
                 p_theta_marg = p_theta_marg/p_theta_marg.sum(axis=1, keepdim=True) # normalize probabilities at each position
                 p_theta_marg = p_theta_marg.to(tgt.device)
                 kl_loss_i = super().forward(p_theta_marg.log(), q_true_minus1)  # KLDivLoss expects input in log-space
-                # todo remove debug
-                print("timestep", timestep[i], kl_loss_i)
-                # if torch.isinf(kl_loss_i): # todo check isnan too
-                #     print(A.shape, B.shape, q_t.shape, p_theta_marg.shape, q_true_minus1.shape)
-                #     for j in range(len(p_theta_marg)):
-                #         kl_loss_j = super().forward(p_theta_marg[j].log(), q_true_minus1[j])
-                #         if torch.isinf(kl_loss_j):
-                #             print(self.tokenizer.untokenize([x_t_tokenized[j]]))
-                #             #print(x_t[j], torch.t(Q[timestep[i]]))
-                #             print(p_theta_marg[j])
-                #             print("ptheta/q_true i", p_theta_marg[j].log(), q_true_minus1[j]) # TODO lets just make this MSA
-                #             print("KL LOSS i", super().forward(p_theta_marg[j].log(), q_true_minus1[j]))
-                #     raise Exception
                 losses.append(kl_loss_i)
         losses = torch.stack(losses)
-        lvb = ((losses.sum()) / (tgt.shape[0]))  # loss per batch, norm by batchsize
-        #print(lvb)
+        lvb = losses.sum() #((losses.sum()) / (tgt.shape[0]))  # loss per batch, norm by batchsize
         return lvb
 
 
@@ -192,7 +172,6 @@ class D3PMCELossMSA(CrossEntropyLoss):
         p_unpadded = p_unpadded.reshape(-1, tokens)
         t_unpadded = torch.masked_select(tgt, nonpad_loc)
         ce_loss = super().forward(p_unpadded, t_unpadded)
-        #[print("ce", ce_loss) # TODO why is CE loss so large?
         return ce_loss
 
 

train.py
@@ -14,14 +14,14 @@ from torch.utils.data import DataLoader
 import torch.distributed as dist
 from torch.cuda.amp import GradScaler
 
-from model import ByteNetLMTime
+from dms.model import ByteNetLMTime
 from dms.utils import Tokenizer
 from torch.utils.data import Subset
 from sequence_models.samplers import SortishSampler, ApproxBatchSampler
 from sequence_models.datasets import UniRefDataset
 from sequence_models.constants import MSA_ALPHABET
 from dms.collaters import OAMaskCollater, D3PMCollater
-from losses import MaskedCrossEntropyLoss, D3PMCELoss, D3PMLVBLoss
+from dms.losses import AutoregMaskedCrossEntropyLoss, D3PMCELoss, D3PMLVBLoss
 from sequence_models.metrics import MaskedAccuracy
 from sequence_models.utils import warmup, transformer_lr
 import sys
@@ -62,7 +62,7 @@ def main():
     parser.add_argument('--warmup', action='store_true')  # Set to True if running on subset of data
     parser.add_argument('--checkpoint_freq', type=float, default=1)  # in minutes
     parser.add_argument('--log_freq', type=float, default=10)  # in steps
-    parser.add_argument('--reweighting_term', type=float, default=0.01)  # lambda reweighting term from Austin D3PM
+    parser.add_argument('--reweighting_term', type=float, default=1)  # lambda reweighting term from Austin D3PM
 
     args = parser.parse_args()
     args.world_size = args.gpus * args.nodes
@@ -70,7 +70,7 @@ def main():
         pass
     else:
         os.environ['MASTER_ADDR'] = 'localhost'
-        os.environ['MASTER_PORT'] = '8890'
+        os.environ['MASTER_PORT'] = '8889'
 
     mp.spawn(train, nprocs=args.gpus, args=(args,))
 
@@ -78,7 +78,6 @@ def train(gpu, args):
     _ = torch.manual_seed(0)
     if args.aml:
         args.nr = int(os.environ['OMPI_COMM_WORLD_RANK'])
-        #print(args.nr)
     rank = args.nr * args.gpus + gpu
     dist.init_process_group(
         backend='nccl',
@@ -236,7 +235,7 @@ def train(gpu, args):
     if args.warmup:
         scheduler = LambdaLR(optimizer, warmup(warmup_steps), verbose=False)
     if args.mask == 'autoreg':
-        loss_func = MaskedCrossEntropyLoss(reweight=True)
+        loss_func = AutoregMaskedCrossEntropyLoss(reweight=True)
     elif args.mask == 'blosum' or args.mask == 'random':
         # Austin = LVB + lambda * CE
         loss_func1 = D3PMLVBLoss(tmax=diffusion_timesteps)
@@ -261,6 +260,7 @@ def train(gpu, args):
         nll_losses = []
         accus = []
         ns = []
+        num_seqs = []
         chunk_time = datetime.now()
         n_seen = 0
         tokens_trained = current_tokens
@@ -293,11 +293,19 @@ def train(gpu, args):
             nll_losses.append(new_nll_loss.item())
             accus.append(new_accu.item())
             ns.append(new_n.item())
+            num_seqs.append(new_seqs.item())
             n_seen += new_seqs.item()
             total_n = sum(ns)
-            r_loss = sum(losses) / total_n
-            r_ce_loss = sum(ce_losses) / total_n
-            r_nll_loss = sum(nll_losses) / total_n
+            total_s = sum(num_seqs)
+            #print(total_n, sum(losses), sum(ce_losses), sum(nll_losses))
+            if args.mask == 'autoreg':
+                r_loss = sum(losses) / total_n
+                r_ce_loss = sum(ce_losses) / total_n
+                r_nll_loss = sum(nll_losses) / total_n
+            elif args.mask == 'blosum' or args.mask == 'random':
+                r_loss = sum(losses)
+                r_ce_loss = sum(ce_losses)
+                r_nll_loss = sum(nll_losses)
             raccu = sum(accus) / total_n
             if train:
                 nsteps = current_step + i + 1
@@ -368,16 +376,6 @@ def train(gpu, args):
         else:
             src, timestep, tgt, mask = batch
             mask = mask.to(device)
-        # ## TODO DELETE LATER
-        # seqs = ""
-        # for t in tgt:
-        #     temp = tokenizer.untokenize(t)
-        #     seqs += (str(temp) + '\n')
-        # with open(args.out_fpath + 'seqs.csv', 'a') as f:
-        #     f.write(','.join(
-        #         [seqs]))
-        #     f.write('\n')
-        ## TODO END
         print("Batchsize", len(timestep))
         timestep = timestep.to(device)
         src = src.to(device)
@@ -388,21 +386,23 @@ def train(gpu, args):
         else:
             n_tokens = mask.sum()
         n_processed = input_mask.sum()
+        n_seqs = torch.tensor(len(src), device=device)
         if train:
             optimizer.zero_grad() # reset gradients of model parameters
         # Enables autocasting for the forward pass (model + loss)
         with torch.cuda.amp.autocast():
             outputs = model(src, timestep, input_mask=input_mask.unsqueeze(-1))
             if args.mask == 'blosum' or args.mask == 'random':
-                lvb_loss = loss_func1(src, q, q_minus1, outputs, tgt, input_mask, timestep, Q, Q_prod) * n_tokens
-                ce_loss = loss_func2(outputs, tgt, input_mask) * n_tokens
+                lvb_loss = loss_func1(src, q, q_minus1, outputs, tgt, input_mask, timestep, Q, Q_prod) # loss
+                ce_loss = loss_func2(outputs, tgt, input_mask) # loss per batch
                 nll_loss = ce_loss
                 loss = lvb_loss + _lambda * ce_loss
+                print(lvb_loss)
+                print(ce_loss, nll_loss)
+                print(loss)
                 accu = accu_func(outputs, tgt, input_mask) * n_tokens
             elif args.mask == 'autoreg':
-                ce_loss, nll_loss = loss_func(outputs, tgt, mask, timestep, input_mask)
-                ce_loss = ce_loss * n_tokens
-                nll_loss = nll_loss * n_tokens
+                ce_loss, nll_loss = loss_func(outputs, tgt, mask, timestep, input_mask) # sum(loss per token) over entire batch
                 loss = ce_loss
                 accu = accu_func(outputs, tgt, mask) * n_tokens
         if train:
@@ -414,7 +414,6 @@ def train(gpu, args):
             skip_scheduler = (scale > scaler.get_scale())
             if not skip_scheduler:
                 scheduler.step()
-        n_seqs = torch.tensor(len(src), device=device)
         return loss, ce_loss, nll_loss, accu, n_tokens, n_seqs, n_processed
 
     if rank == 0: