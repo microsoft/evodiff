a148e9c91c47bb10c0bd1f5f5b9e8c4e6f012fa5
v-salamdari <sarahhalam@gmail.com>
2022-10-12 10:33:01 -0400

    clean

--- dms/constants.py
+++ dms/constants.py
@@ -1,23 +1,8 @@
-from sequence_models.constants import AAINDEX_ALPHABET, AMB_AAS, OTHER_AAS, GAP, MSA_PAD, STOP, MASK, START
+from sequence_models.constants import AAINDEX_ALPHABET, AMB_AAS, OTHER_AAS, GAP, STOP, MASK, START, MSA_PAD,\
+                                      MSA_ALPHABET, MSA_AAS
 
-# ORDER is important for BLOSUM indexing
 
 # FOR BLOSUM MATRIX CREATION ONLY (In order of BLOSUM indices for mapping onto MSA_ALPHABET)
 BLOSUM_AAS = AAINDEX_ALPHABET + AMB_AAS
 BLOSUM_ALPHABET = BLOSUM_AAS + OTHER_AAS + GAP + MSA_PAD + STOP + MASK + START
 BLOSUM_EXTRAS = 'JOU-'
-
-
-### CONSTANTS RELEVANT TO PROTEIN VOCAB ###
-# CAN_AAS = 'ACDEFGHIKLMNPQRSTVWY'
-# # single AA letter codes
-#
-# AMB_AAS = 'BZX'
-# # B: aspartic acid (D) or asparagine (N)
-# # Z: glu acid (E) or glutamine (Q)
-# # X: any
-#
-# OTHER_AAS = 'JOU' # AND THESE
-# # J: leucine (L) or isoleucine (I)
-# # O: Pyrrolysine
-# # U: Selenocystine
