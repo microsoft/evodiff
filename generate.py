17ad2dcbf7e1d764c1ab87fc61c28237daff6346
v-salamdari <sarahhalam@gmail.com>
2022-11-17 11:09:26 -0500

    batch generation for sequences

--- generate.py
+++ generate.py
@@ -7,6 +7,7 @@ import os
 import json
 from dms.utils import Tokenizer
 import pathlib
+from tqdm import tqdm
 
 ### SET RANDOM SEEDS ####
 random_seed = 1
@@ -31,6 +32,9 @@ def main():
                         help='number of gpus per node')
     parser.add_argument('--no_step', action='store_true') # For D3PM if true will predict x_0 from x_t, instead of x_tminus1
     args = parser.parse_args()
+
+    _ = torch.manual_seed(0)
+    np.random.seed(0)
 
     with open(args.config_fpath, 'r') as f:
         config = json.load(f)
@@ -59,7 +63,7 @@ def main():
     causal = False
     n_tokens = len(MSA_ALPHABET)
 
-    if args.mask == 'autoreg' or args.mask == 'so':
+    if args.mask == 'autoreg' or args.mask == 'so' or args.mask == 'reference':
         tokenizer = Tokenizer()
         diffusion_timesteps = None  # Not input to model
         if args.mask == 'so':
@@ -67,7 +71,7 @@ def main():
             tokenizer = Tokenizer(protein_alphabet=PROTEIN_ALPHABET, all_aas=ALL_AAS, pad=PAD)
             causal = True
     elif args.mask == 'blosum' or args.mask == 'random':
-        tokenizer = Tokenizer(path_to_blosum=data_top_dir + "blosum62-special-MSA.mat")
+        tokenizer = Tokenizer(path_to_blosum=data_top_dir + "blosum62-special-MSA.mat", sequences=True)
         diffusion_timesteps = config['diffusion_timesteps']
         if args.mask == 'random':
             Q_prod, Q_t = tokenizer.q_random_schedule(timesteps=diffusion_timesteps)
@@ -84,7 +88,7 @@ def main():
     model = ByteNetLMTime(n_tokens, d_embed, d_model, n_layers, kernel_size, r,
                           causal=causal, padding_idx=masking_idx, rank=weight_rank, dropout=args.dropout,
                           tie_weights=args.tie_weights, final_ln=args.final_norm, slim=slim, activation=activation,
-                          timesteps=diffusion_timesteps)
+                          timesteps=diffusion_timesteps) # works w/ time and non-time models (when diffusion_timesteps is None)
 
     if args.checkpoint is not None:
         last_epoch = args.checkpoint
@@ -100,73 +104,67 @@ def main():
                        args.state_dict = args.out_fpath + output
                        last_epoch = epoch
 
-    print('Using checkpoint', last_epoch)
-    print('Loading weights from ' + args.state_dict + '...')
-    sd = torch.load(args.state_dict, map_location=torch.device('cpu'))
-    msd = sd['model_state_dict']
-    #print(list(msd.keys())[0:10])
-    if args.mask == 'so':
-        msd = {k.split('module.')[1]: v for k, v in msd.items()}
-    else:
-        msd = {k.split('module.')[0]: v for k,v in msd.items()}
-    #print(list(msd.keys())[0:10], list(model.state_dict().keys())[0:10])
-    model.load_state_dict(msd) # TODO: why is this not saving the same
+    if args.mask != 'reference':
+        print('Using checkpoint', last_epoch)
+        print('Loading weights from ' + args.state_dict + '...')
+        sd = torch.load(args.state_dict, map_location=torch.device('cpu'))
+        msd = sd['model_state_dict']
+        #print(list(msd.keys())[0:10])
+        if args.mask == 'so':
+            msd = {k.split('module.')[1]: v for k, v in msd.items()}
+        else:
+            msd = {k.split('module.')[0]: v for k,v in msd.items()}
+        #print(list(msd.keys())[0:10], list(model.state_dict().keys())[0:10])
+        model.load_state_dict(msd)
 
     sequences = args.num_seqs
-    # The following were calculated from uniref dataset #
-    seq_lengths = [64, 166, 268, 369, 471, 573, 675, 776, 878, 980, 1081, 1183, 1285, 1386, 1488, 1590, 1692, 1793,
-                   1895, 1997]
-    freq = [1, 0.8879753340184995, 0.5765673175745119, 0.35765673175745116, 0.19013360739979446, 0.131551901336074, \
-            0.0698869475847893, 0.06063720452209661, 0.041109969167523124, 0.030832476875642344, 0.01644398766700925, \
-            0.010277492291880781, 0.008221993833504625, 0.004513874614594039, 0.0067831449126413155, \
-            0.009660842754367934, 0.003083247687564234, 0.0018633093525179856, 0.0043031860226104834, \
-            0.02158273381294964]
-    ###
-    samples = [int(f * sequences) for f in freq]
-    print(len(samples))
-    print(len(seq_lengths))
+    seq_lengths = [32, 64, 128, 256, 384, 512] #, 1024, 2048]
     seqs = ""
-    with open(args.out_fpath + 'generated_samples_string.csv', 'a') as f:
-        for i, seq_len in enumerate(seq_lengths):
-            sequences = samples[i]
-            if (samples[i] != 0):
-                for j in range(sequences):
-                    print("sample", j)
-                    if args.mask == 'autoreg' or args.mask=='so':
-                        sample, string = generate_text(model, seq_len, tokenizer=tokenizer, penalty=args.penalty, causal=causal)
-                        seqs += str(string)
-                        seqs += "\n"
-                    elif args.mask == 'blosum' or args.mask == 'random':
-                        sample, string = generate_text_d3pm(model, seq_len, Q_bar=Q_prod, Q=Q_t,
-                                                            timesteps=diffusion_timesteps, no_step=args.no_step)
-                        seqs += str(string)
-                        seqs += "\n"
-                    f.write(str(string) + "\n")
-        f.close()
 
-    #np.savetxt(args.out_fpath + 'generated_samples.csv', seqs_array, delimiter=',', fmt='%d') # not necessary
+    for i, seq_len in enumerate(seq_lengths):
+        with open(args.out_fpath + 'generated_samples_string_'+str(seq_len)+'.fasta', 'a') as f:
+            count = 0
+            fasta_string = ""
+
+            if args.mask == 'autoreg' or args.mask=='so':
+                sample, string = generate_text(model, seq_len, tokenizer=tokenizer, penalty=args.penalty, causal=causal,
+                                               batch_size=args.num_seqs)
+            elif args.mask == 'blosum' or args.mask == 'random':
+                sample, string = generate_text_d3pm(model, seq_len, Q_bar=Q_prod, Q=Q_t, tokenizer=tokenizer,
+                                                    timesteps=diffusion_timesteps, no_step=args.no_step,
+                                                    batch_size=args.num_seqs)
+            elif args.mask == 'reference':
+                sample, string = generate_random_seq(seq_len, tokenizer=tokenizer)
+
+            for _s in string:
+                fasta_string += ">SEQUENCE_" + str(count) + "\n" + str(_s) + "\n"
+                count += 1
+                seqs += str(_s) + "\n"
+
+            f.write(fasta_string)
+            f.close()
+
     with open(args.out_fpath + 'generated_samples_string.csv', 'a') as f:
         f.write(','.join(
             [seqs]))
         f.write('\n')
 
-def generate_text(model, seq_len, tokenizer=Tokenizer(), penalty=None, causal=False):
+def generate_text(model, seq_len, tokenizer=Tokenizer(), penalty=None, causal=False, batch_size=20):
     # Generate a random start string and convert to tokens
     all_aas = tokenizer.all_aas
     mask = tokenizer.mask_id
     # Start from mask
-    sample = torch.zeros((seq_len))+mask
+    sample = torch.zeros((batch_size, seq_len))+mask
     sample = sample.to(torch.long)
-    print("input seq", tokenizer.untokenize(sample))
+    #print("input seq", tokenizer.untokenize(sample))
     # Unmask 1 loc at a time randomly
     loc = np.arange(seq_len)
-    print(len(loc))
     if causal == False:
         np.random.shuffle(loc)
     with torch.no_grad():
         for i in loc:
-            timestep = torch.tensor([0]) # placeholder not called
-            prediction = model(sample.unsqueeze(0), timestep) #, input_mask=input_mask.unsqueeze(-1)) #sample prediction given input
+            timestep = torch.tensor([0] * batch_size) # placeholder but not called in model
+            prediction = model(sample, timestep) #, input_mask=input_mask.unsqueeze(-1)) #sample prediction given input
             p = prediction[:, i, :len(all_aas)-6] # sample at location i (random), dont let it predict non-standard AA
             p = torch.nn.functional.softmax(p, dim=1) # softmax over categorical probs
             p_sample = torch.multinomial(p, num_samples=1)
@@ -179,48 +177,68 @@ def generate_text(model, seq_len, tokenizer=Tokenizer(), penalty=None, causal=Fa
                     #print("identified repeat", p_sample, sample[i-1], sample[i+1])
                     p[:, int(p_sample)] /= penalty
                     p_sample = torch.multinomial(p, num_samples=1) # resample
-            sample[i] = p_sample
-            #print(tokenizer.untokenize(sample)) # check that sampling correctly
-    print("final seq", tokenizer.untokenize(sample))
-    return sample, tokenizer.untokenize(sample)
+            sample[:, i] = p_sample.squeeze()
+            #print([tokenizer.untokenize(s) for s in sample]) # check that sampling correctly
 
-def generate_text_d3pm(model, seq_len, Q_bar=None, Q=None, tokenizer=Tokenizer(), timesteps=500, no_step=False):
+    print("final seq", [tokenizer.untokenize(s) for s in sample])
+    untokenized = [tokenizer.untokenize(s) for s in sample]
+    return sample, untokenized
+
+def generate_text_d3pm(model, seq_len, Q_bar=None, Q=None, tokenizer=Tokenizer(), timesteps=500, no_step=False,
+                       batch_size=20):
     """
-    no_step: if true will calculate p_tilde(x_0|x_t)
+    no_step: if true will calculate p_tilde(x_0|x_t) from a uniform sample
              if false will calculate p_tilde(x_tminus1|x_t) for each t in timestep
     """
     # Generate a random start string from uniform dist and convert to tokens
-    all_aas = tokenizer.all_aas
-    sample = torch.randint(0, len(all_aas), (seq_len,))
+    #all_aas = tokenizer.all_aas
+
+    sample = torch.randint(0, tokenizer.K, (batch_size, seq_len)) # don't include gap token?
     sample = sample.to(torch.long)
-    print("input seq", tokenizer.untokenize(sample))
+    #print("input seq", tokenizer.untokenize(sample))
     if no_step:
-        timesteps = torch.linspace(timesteps-1, timesteps-1, steps=1, dtype=torch.long)
+        timesteps = np.linspace(timesteps-1, timesteps-1, steps=1, dtype=int)
     else:
-        timesteps = torch.linspace(timesteps-1,1,timesteps-1) # iterate over reverse timesteps
-        timesteps = timesteps.to(torch.long)
+        timesteps = np.linspace(timesteps-1,1,int((timesteps-1)/1), dtype=int) # iterate over reverse timesteps
     with torch.no_grad():
         for t in timesteps:
-            x_t = tokenizer.one_hot(sample)
-            prediction = model(sample.unsqueeze(0), t.unsqueeze(0))
-            p = prediction[:, :, :len(all_aas)]  # p_theta_tilde (x_0_tilde | x_t)
+            timesteps = [t] * batch_size
+            prediction = model(sample, torch.tensor(timesteps))
+            p = prediction[:, :, :tokenizer.K]  # p_theta_tilde (x_0_tilde | x_t)
             p = torch.nn.functional.softmax(p, dim=2).squeeze()  # softmax over categorical probs
             p = p.to(torch.float64)
             if no_step:
-                x_tminus1 = torch.multinomial(p, num_samples=1).squeeze()
+                x_tminus1 = torch.multinomial(p, num_samples=1)
             else:
-                # Calculate p_theta_marg from p_theta_tilde
-                A = torch.mm(x_t, torch.t(Q[t]))  # [P x K]
-                B = Q_bar[t - 1] # [K, K]
-                q_t = torch.mul(A.unsqueeze(1), B)  # [ P x K x K ]
-                p_theta_marg = torch.bmm(q_t, p.unsqueeze(2)).squeeze()  # this marginalizes over dim=2
-                p_theta_marg = p_theta_marg / p_theta_marg.sum(axis=1, keepdim=True)
-                x_tminus1 = torch.multinomial(p_theta_marg, num_samples=1).squeeze()
-            diff = torch.ne(sample, x_tminus1)
-            if t % 50 == 0:
-                print("time", t, diff.sum().item(), "mutations", tokenizer.untokenize(x_tminus1))
+                x_tminus1 = sample.clone()
+                for i, s in enumerate(sample):
+                    # Calculate p_theta_marg from p_theta_tilde
+                    x_t_b = tokenizer.one_hot(s)
+                    A = torch.mm(x_t_b, torch.t(Q[t]))  # [P x K]
+                    Q_expand = Q_bar[t-1].unsqueeze(0).expand(A.shape[0], tokenizer.K, tokenizer.K)  # [ P x K x K]
+                    B_pred = torch.mul(p[i].unsqueeze(2), Q_expand)
+                    q_t = torch.mul(A.unsqueeze(1), B_pred)  # [ P x K x K ]
+                    p_theta_marg = torch.bmm(q_t,  p[i].unsqueeze(2)).squeeze()  # this marginalizes over dim=2
+                    p_theta_marg = p_theta_marg / p_theta_marg.sum(axis=1, keepdim=True)
+                    x_tminus1[i] = torch.multinomial(p_theta_marg, num_samples=1).squeeze()
+                    diff = torch.ne(s, x_tminus1[i])
+                    if t % 20 == 0:
+                        print("time", t, diff.sum().item(), "mutations") #, tokenizer.untokenize(x_tminus1))
             sample = x_tminus1
-    print("final seq", tokenizer.untokenize(sample))
+    untokenized = [tokenizer.untokenize(s) for s in sample]
+    print("final seq", untokenized)
+    return sample, untokenized
+
+def generate_random_seq(seq_len, tokenizer=Tokenizer()):
+    """
+    Generates a set of random sequences drawn from a uniform distribution
+    """
+    # Generate a random start string from uniform dist and convert to tokens
+    all_aas = tokenizer.all_aas
+    print(all_aas)
+    sample = torch.randint(0, len(all_aas)-4, (seq_len,)) # ignore char not accepted by PROSITE
+    sample = sample.to(torch.long)
+    print("sequence", tokenizer.untokenize(sample))
     return sample, tokenizer.untokenize(sample)
 
 if __name__ == '__main__':
