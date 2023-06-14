2fada0d3966390bd07b91467aab60f0b02076db8
v-salamdari <sarahhalam@gmail.com>
2023-06-14 11:07:33 -0400

    analysis for train/test/valid splits

--- analysis/plots_traindata.py
+++ analysis/plots_traindata.py
@@ -25,16 +25,14 @@ data_dir = data_top_dir + dataset + '/'
 
 metadata = np.load(data_dir + 'lengths_and_offsets.npz')
 ds_train = UniRefDataset(data_dir, 'train', structure=False)
+ds_valid = UniRefDataset(data_dir, 'test', structure=False) # Using test for analysis
+valid_idx = ds_valid.indices
 train_idx = ds_train.indices
-
 len_train = metadata['ells'][train_idx]
 train_sortish_sampler = SortishSampler(len_train, bucket_size, num_replicas=1, rank=0)
 
-#mini_size = 100
-#tindices = np.arange(21546293,31546293,1)#(1000000,21546293, 1)
-#train_indices = np.sort(np.random.choice(tindices, mini_size, replace=False))
-#train_sampler = Subset(ds_train,train_indices)
 train_sampler = ApproxBatchSampler(train_sortish_sampler, max_tokens, max_batch_size, len_train)
+valid_sampler = Subset(ds_valid, valid_idx)
 # dl_train = DataLoader(dataset=train_sampler,
 #                               shuffle=True,
 #                               batch_size=20,
@@ -42,29 +40,33 @@ train_sampler = ApproxBatchSampler(train_sortish_sampler, max_tokens, max_batch_
 dl_train = DataLoader(dataset=ds_train,
                           batch_sampler=train_sampler,
                           num_workers=16)
-# valid_sortish_sampler = SortishSampler(len_train, 80000, num_replicas=1, rank=0)
-# valid_sampler = ApproxBatchSampler(valid_sortish_sampler, max_tokens // 2, max_batch_size, len_train)
-# dl_train = DataLoader(dataset=ds_train,
-#                       batch_sampler=valid_sampler,
-#                       num_workers=8)
-#letters = Counter({})
+
+len_valid = metadata['ells'][valid_idx]
+valid_sortish_sampler = SortishSampler(len_valid, 1000, num_replicas=1, rank=0)
+valid_sampler = ApproxBatchSampler(valid_sortish_sampler, max_tokens // 2, max_batch_size, len_valid)
+dl_valid = DataLoader(dataset=ds_valid,
+                  batch_sampler=valid_sampler,
+                  num_workers=8)
+
 aminos = Counter({'A':0, 'M':0, 'R':0, 'T':0, 'D':0, 'Y':0, 'P':0, 'F':0, 'L':0, 'E':0, 'W':0, 'I':0, 'N':0, 'S':0,\
                       'K':0, 'Q':0, 'H':0, 'V':0, 'G':0, 'C':0, 'X':0, 'B':0, 'Z':0, 'J':0, 'O':0, 'U':0})
 seq_len = []
 
-for i, batch in enumerate(dl_train):
+#for i, batch in enumerate(dl_train):
+#print(len(dl_valid))
+for i, batch in enumerate(dl_valid):
     for seq in batch[0]:
         aminos.update(seq)
         print(len(seq))
         seq_len.append(len(seq))
     if i % 1000 == 0 :
         print("writing batch ", i)
-        with open('count2/batch-count-valid'+str(i)+'.csv', 'w') as file:
+        with open('count3/batch-count-test'+str(i)+'.csv', 'w') as file:
             writer = csv.DictWriter(file, aminos.keys())
             writer.writeheader()
             writer.writerow(aminos)
 
-        with open('count2/seq_len-valid'+str(i)+'.csv', 'w') as file2:
+        with open('count3/seq_len-test'+str(i)+'.csv', 'w') as file2:
             for item in seq_len:
                 # write each item on a new line
                 file2.write("%s\n" % item)
