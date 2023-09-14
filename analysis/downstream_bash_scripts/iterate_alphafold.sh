7d31e12c0d7fe631902209b527b0cedfde313efa
v-salamdari <sarahhalam@gmail.com>
2023-09-14 13:08:31 -0400

    update

--- analysis/downstream_bash_scripts/iterate_alphafold.sh
+++ analysis/downstream_bash_scripts/iterate_alphafold.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-NUM_SEQS=51 # +1 since indexed at 1
-run='/home/v-salamdari/Desktop/DMs/amlt-generate/diff-oaardm-msa-random'
-seqs='valid_query' # or gen_query
+# Generate MSA for sequence, and get alphafold prediction
 
-for ((i=1; i<=$NUM_SEQS; i++ )); do
-  echo $i
-  python3 docker/run_docker.py \
-    --fasta_paths=$run/gen-$i/$seqs.fasta \
-    --data_dir=/data/ALPHAFOLD_DOWNLOAD_DIR/ \
-    --output_dir=$run/gen-$i/ \
-    --max_template_date=2023-05-05 \
-    --use_precomputed_msas=True
+strings=(
+        #1qjg
+        #1prw
+        #1bcf
+        #5tpn
+        #5wn9
+        #5ius
+        #1ycr
+        #7p19
+        #2kl8
+        #7mrx
+        #5trv
+        #6exz
+        #6e6r
+        #6vw1
+        #4jhw
+        #4zyp
+        #3ixt
+        4qvf
+        2xa0
+        )
 
+DOWNLOAD_DIR=/data/ALPHAFOLD_DOWNLOAD_DIR
+output_dir=/home/v-salamdari/Desktop/DMs/cond-gen/scaffolding-pdbs/
+
+for i in "${strings[@]}"; do
+
+fasta_path=/home/v-salamdari/Desktop/DMs/cond-gen/scaffolding-pdbs/${i}.fasta
+
+python3 docker/run_docker.py \
+  --fasta_paths=$fasta_path \
+  --max_template_date=2023-07-13 \
+  --data_dir=$DOWNLOAD_DIR \
+  --output_dir=$output_dir
+  --models_to_relax=best
+  --num_multimer_predictions_per_model=1
 done
